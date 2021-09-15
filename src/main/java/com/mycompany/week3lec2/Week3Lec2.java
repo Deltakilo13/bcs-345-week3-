90afdd6487b08d5ec5cc53b75eb496ccd6c7886f
dkuzm <dkuzm@137.125.132.146>
2021-09-15 10:19:27 -0400

    Initial commit - Even project

---
+++ src/main/java/com/mycompany/week3lec2/Week3Lec2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.week3lec2;
+
+/**
+ *
+ * @author dkuzm
+ */
+public class Week3Lec2 {
+    
+    static int getSum(int a, int b){
+        return a+b;
+        
+    }
+    
+     public static void main(String[]args) {
+       
+        int x=10, y=100;
+        int z=getSum(x,y); 
+        System.out.println("the sum is "+ z);
+    }
+}
